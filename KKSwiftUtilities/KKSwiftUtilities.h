e38858f8ebdfb818c6dae970d4c1655ed62f5a7c
Kakhi Kiknadze <kakhi.kiknadze@wandio.com>
2020-09-20 14:07:41 +0400

    Initial Commit

---
+++ KKSwiftUtilities/KKSwiftUtilities.h
@@ -0,0 +1,18 @@
+//
+//  KKSwiftUtilities.h
+//  KKSwiftUtilities
+//
+//  Created by Kakhi Kiknadze on 9/20/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KKSwiftUtilities.
+FOUNDATION_EXPORT double KKSwiftUtilitiesVersionNumber;
+
+//! Project version string for KKSwiftUtilities.
+FOUNDATION_EXPORT const unsigned char KKSwiftUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKSwiftUtilities/PublicHeader.h>
+
+
